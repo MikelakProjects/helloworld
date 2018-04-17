444624c0957c5fc88b06559cf4fbe24bedcb7c86
Mikelak <mikelak.projects@gmail.com>
2018-04-17 22:36:28 +0200

    Comentarios

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,7 +1,9 @@
 package helloworld;
 
 /**
- * Aquí hace falta sofisticar un poco el código. 
+ * Aquí hace falta sofisticar un poco el código. ¿Qué ocurre si queremos cambiar el mensaje? ¿O si queremos que la salida
+ * vaya por error en lugar de out? ¿O mostrar el texto con otro formato?
+ * 
  * @author mikelak
  *
  */
