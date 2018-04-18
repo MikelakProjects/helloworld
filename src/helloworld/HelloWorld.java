eed1acfe20312e12699a54e3cac8404f2b94ddf9
Mikelak <mikelak.projects@gmail.com>
2018-04-18 21:20:09 +0200

    Factory para desacoplar

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,9 +1,7 @@
 package helloworld;
 
 /**
- * Para separar la aplicación en dos componentes, creamos dos interfaces y las clases que las implementan. 
- * No obstante, sigue habiendo un problema. Si queremos cambiar la implementación, hemos de cambiar el código,
- * forzosamente. Para evitar esto, crearemos una clase Factory (en la rama3)
+ * Hemos desacoplado el código de la implementación particular, definiéndola en un fichero .properties
  * @author mikelak
  *
  */
@@ -11,8 +9,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {	
 		
-		MessageRenderer mr = new StandardOutMessageRenderer();
-		MessageProvider mp = new HelloWorldMessageProvider();
+		MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer();
+		MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider();
 		
 		mr.setMessageProvider(mp);
 		mr.render();
---
+++ src/helloworld/MessageSupportFactory.java
@@ -0,0 +1,55 @@
+package helloworld;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+/**
+ * Con esta clase Factory conseguimos que se carguen las implementaciones que queremos con sólo modificar el fichero
+ * .properties.
+ * @author mikelak
+ *
+ */
+
+public class MessageSupportFactory {
+
+	// Patrón Singleton
+	private static MessageSupportFactory instance;
+	
+	private Properties props;
+	private MessageRenderer renderer;
+	private MessageProvider provider;
+	
+	private MessageSupportFactory() {
+		props = new Properties();
+		
+		try {
+			props.load(new FileInputStream("src/resources/msf.properties"));
+			
+			String rendererClass = props.getProperty("renderer.class");
+			String providerClass = props.getProperty("provider.class");
+			
+			renderer = (MessageRenderer) Class.forName(rendererClass).newInstance();
+			provider = (MessageProvider) Class.forName(providerClass).newInstance();
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	// Patrón Singleton
+	static {
+		instance = new MessageSupportFactory();
+	}
+	
+	public static MessageSupportFactory getInstance() {
+		return instance;
+	}
+	
+	public MessageRenderer getMessageRenderer() {
+		return renderer;
+	}
+	
+	public MessageProvider getMessageProvider() {
+		return provider;
+	}
+}
