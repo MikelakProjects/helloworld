29e9a3202b6f3f69d065c92f896e99203db65c42
Mikelak <mikelak.projects@gmail.com>
2018-04-18 20:31:41 +0200

    Segunda versión

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,15 +1,24 @@
 package helloworld;
 
 /**
- * Aquí hace falta sofisticar un poco el código. ¿Qué ocurre si queremos cambiar el mensaje? ¿O si queremos que la salida
- * vaya por error en lugar de out? ¿O mostrar el texto con otro formato?
+ * Hacemos que sea algo más configurable (extensible dicen en el libro) pintando el valor que le pasemos por parámetro
+ * de entrada, y si no se pasa ninguno, pintará el mensaje por defecto.
+ * 
+ * No obstante, se podría hacer mucho más "extensible". Por ejemplo, el componente que pinta el mensaje es el mismo
+ * que lo obtiene. Cambiar uno supone cambiar el otro. Un mejor diseño será separar ambos componentes.
  * 
  * @author mikelak
  *
  */
 public class HelloWorld {
 
-	public static void main(String[] args) {		
-		System.out.println("Hello, world!");
+	public static void main(String[] args) {	
+		
+		if (args.length > 0) {
+			System.out.println(args[0]);
+		}
+		else {
+			System.out.println("Hello, world!");
+		}		
 	}
 }
