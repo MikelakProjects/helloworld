4330f0e937db99095a19eb2e6b4a79b1d80cac6f
Mikelak <mikelak.projects@gmail.com>
2018-08-08 22:44:58 +0200

    Segunda prueba tag

--- src/helloworld/MessageProvider.java
+++ src/helloworld/MessageProvider.java
@@ -3,7 +3,7 @@ package helloworld;
 /**
  * Esta interfaz define la "forma" del componente que obtendrá el mensaje.     
  * @author mikelak
- *
+ * Ahora añado esta mierda para ver cómo se comportan los commits con los tags en git
  */
 public interface MessageProvider {
 
